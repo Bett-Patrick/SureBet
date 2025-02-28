bfb07a8f512198116d89e16809c6890408a4e05b
Bett <bettpatrick080@gmail.com>
2025-02-28 09:47:39 +0300

    Managed user login state using onAuthStateChanged

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import Layout from "../public/Components/Layout";
 import PredictionsPage from "./Pages/PredictionsPage";
@@ -6,8 +6,16 @@ import Register from "./Pages/Register";
 import Login from "./Pages/Login";
 import { ToastContainer } from "react-toastify";
 import UserProfile from "./Pages/UserProfile";
+import { useEffect, useState } from "react";
+import { auth } from "../public/Components/firebase";
 
 const App = () => {
+  const [user, setUser] = useState();
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setUser(user);
+    })
+  },[])
 
   return (
     <BrowserRouter>
@@ -15,7 +23,7 @@ const App = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={user ? <Navigate to="/profile" /> : <Login />} />
           <Route path="/profile" element={<UserProfile />} />
           <Route path="/predictions" element={<PredictionsPage />} />
         </Route>
