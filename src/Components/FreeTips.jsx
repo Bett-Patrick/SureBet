47e13abadde7227c767cb5c20c4c8887144d8f74
Bett <bettpatrick080@gmail.com>
2025-03-18 18:41:09 +0300

    did away with betting sites odds

--- src/Components/FreeTips.jsx
+++ src/Components/FreeTips.jsx
@@ -21,97 +21,19 @@ const FreeTips = () => {
                         <div className="border w-[100px] my-2 rounded-md">Prediction</div>
                         <div className="statistics flex justify-baseline gap-1 text-amber-300 border-2 border-transparent cursor-pointer"><FiBarChart2 className="text-amber-300 text-2xl"/> <span className="pt-0.5 hover:text-white text-md tracking-widest font-mono hover:font-extrabold hover:border-b-2">Statistics</span></div>
                     </aside>
-                    {/* <h1 className="text-md font-bold text-amber-300">ODDS</h1> */}
+
                     <hr />
-                    <aside className="odds-container my-2">
-                        <aside className="odds flex flex-row gap-10 justify-center items-center">
-                            <div className="border w-[100px] my-2 rounded-md">Betika</div>
-                            <div className="border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">1</span>1.25</div>
-                            <div className="border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">x</span>2.25</div>
-                            <div className="border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">2</span>1.98</div>
-                        </aside>
-                        <aside className="odds flex flex-row gap-10 justify-center items-center">
-                            <div className="border w-[100px] my-2 rounded-md">Betway</div>
-                            <div className="border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">1</span>1.25</div>
-                            <div className="border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">x</span>2.25</div>
-                            <div className="border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">2</span>1.90</div>
-                        </aside>
-                        <aside className="odds flex flex-row gap-10 justify-center items-center">
-                            <div className="border w-[100px] my-2 rounded-md">1X BET</div>
-                            <div className="border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">1</span>1.25</div>
-                            <div className="border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">x</span>2.25</div>
-                            <div className="border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">2</span>1.80</div>
-                        </aside>
+
+                    {/* <h1 className="text-md font-bold text-amber-300">ODDS</h1> */}
+                    <aside className="odds-container flex flex-row gap-10 justify-center items-center">
+                        <div className="border w-[100px] my-2 rounded-md text-amber-400 font-semibold">ODDS</div>
+                        <div className="home border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">1</span>1.25</div>
+                        <div className="draw border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">x</span>2.25</div>
+                        <div className="away border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">2</span>1.98</div>
                     </aside>
-                    {/* <hr className="h-[20px] bg-white" /> */}
+                    <hr className="h-[20px] bg-white" />
                 </li>
 
-                <li className="tip w-full mx-auto bg-[#000435]">
-                    <aside className="prediction flex flex-row gap-11 justify-center items-center">
-                        <div className="border w-[100px] my-2 rounded-md">Home Team</div>
-                        <div className="">vs</div>
-                        <div className="border w-[100px] my-2 rounded-md">Away Team</div>
-                        <div className="border w-[100px] my-2 rounded-md">Prediction</div>
-                        <div className="statistics flex justify-baseline gap-1 text-amber-300 border-2 border-transparent cursor-pointer"><FiBarChart2 className="text-amber-300 text-2xl"/> <span className="pt-0.5 hover:text-white text-md tracking-widest font-mono hover:font-extrabold hover:border-b-2">Statistics</span></div>
-                    </aside>
-                    {/* <h1 className="text-md font-bold text-amber-300">ODDS</h1> */}
-                    <hr />
-                    <aside className="odds-container my-2">
-                        <aside className="odds flex flex-row gap-10 justify-center items-center">
-                            <div className="border w-[100px] my-2 rounded-md">Betika</div>
-                            <div className="border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">1</span>1.25</div>
-                            <div className="border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">x</span>2.25</div>
-                            <div className="border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">2</span>1.98</div>
-                        </aside>
-                        <aside className="odds flex flex-row gap-10 justify-center items-center">
-                            <div className="border w-[100px] my-2 rounded-md">Betway</div>
-                            <div className="border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">1</span>1.25</div>
-                            <div className="border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">x</span>2.25</div>
-                            <div className="border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">2</span>1.90</div>
-                        </aside>
-                        <aside className="odds flex flex-row gap-10 justify-center items-center">
-                            <div className="border w-[100px] my-2 rounded-md">1X BET</div>
-                            <div className="border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">1</span>1.25</div>
-                            <div className="border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">x</span>2.25</div>
-                            <div className="border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">2</span>1.80</div>
-                        </aside>
-                    </aside>
-                    {/* <hr className="h-[20px] bg-white" /> */}
-                </li>
-
-                <li className="tip w-full mx-auto bg-[#000435]">
-                    <aside className="prediction flex flex-row gap-11 justify-center items-center">
-                        <div className="border w-[100px] my-2 rounded-md">Home Team</div>
-                        <div className="">vs</div>
-                        <div className="border w-[100px] my-2 rounded-md">Away Team</div>
-                        <div className="border w-[100px] my-2 rounded-md">Prediction</div>
-                        <div className="statistics flex justify-baseline gap-1 text-amber-300 border-2 border-transparent cursor-pointer"><FiBarChart2 className="text-amber-300 text-2xl"/> <span className="pt-0.5 hover:text-white text-md tracking-widest font-mono hover:font-extrabold hover:border-b-2">Statistics</span></div>
-                    </aside>
-                    {/* <h1 className="text-md font-bold text-amber-300">ODDS</h1> */}
-                    <hr />
-                    <aside className="odds-container my-2">
-                        <aside className="odds flex flex-row gap-10 justify-center items-center">
-                            <div className="border w-[100px] my-2 rounded-md">Betika</div>
-                            <div className="border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">1</span>1.25</div>
-                            <div className="border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">x</span>2.25</div>
-                            <div className="border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">2</span>1.98</div>
-                        </aside>
-                        <aside className="odds flex flex-row gap-10 justify-center items-center">
-                            <div className="border w-[100px] my-2 rounded-md">Betway</div>
-                            <div className="border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">1</span>1.25</div>
-                            <div className="border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">x</span>2.25</div>
-                            <div className="border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">2</span>1.90</div>
-                        </aside>
-                        <aside className="odds flex flex-row gap-10 justify-center items-center">
-                            <div className="border w-[100px] my-2 rounded-md">1X BET</div>
-                            <div className="border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">1</span>1.25</div>
-                            <div className="border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">x</span>2.25</div>
-                            <div className="border w-[100px] my-2 rounded-md"><span className="text-gray-500 mr-5 text-sm">2</span>1.80</div>
-                        </aside>
-                    </aside>
-                    {/* <hr className="h-[20px] bg-white" /> */}
-                </li>
-                
             </ul>
         </div>
     </div>
